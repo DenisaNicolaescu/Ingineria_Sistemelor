bf137f7934395e0ef3e7488b38eecad8473c4355
Denisa Nicolaescu <mariadenisa.nicolaescu@ulbsibiu.ro>
2026-03-10 10:35:32 +0200

    Tema 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+
+    public static void main() {
+            Student s1 = new Student(586, "Victoria", "Roman", 22);
+
+            System.out.println(s1);
+
+    }
---
+++ src/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    private int numarMatricol;
+    private String prenume;
+    private String nume;
+    private int formatieDeStudiu;
+
+
+    public Student(int numarMatricol, String prenume, String nume, int formatieDeStudiu) {
+        this.numarMatricol = numarMatricol;
+        this.prenume = prenume;
+        this.nume = nume;
+        this.formatieDeStudiu = formatieDeStudiu;
+    }
+
+
+    public String toString() {
+        return "Student:" + "Nr. Matricol-" + " "+ numarMatricol +" " + "Nume-" +" " + nume + " "+ "Prenume-"+" "+prenume+" "+"Grupa-"+" "+formatieDeStudiu;
+    }
+
+}
