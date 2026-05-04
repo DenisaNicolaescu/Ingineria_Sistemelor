e6bc0ce196a631a0d5bfd9d26edc56ef7891fbbd
Denisa Nicolaescu <mariadenisa.nicolaescu@ulbsibiu.ro>
2026-05-04 14:30:34 +0300

    Tema lab 8

---
+++ src/Lab8/CitireExcel.java
@@ -0,0 +1,46 @@
+package Lab8;
+
+import java.io.IOException;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.File;
+import java.io.FileInputStream;
+
+public class CitireExcel {
+
+    public static void main(String[] args) {
+        String fileName = "laborator8_input.xlsx";
+
+        try (FileInputStream fis = new FileInputStream(new File(fileName));
+             Workbook workbook = new XSSFWorkbook(fis)) {
+
+            Sheet sheet = workbook.getSheetAt(0);
+
+            for (Row row : sheet) {
+                for (Cell cell : row) {
+                    switch (cell.getCellType()) {
+                        case STRING:
+                            System.out.print(cell.getStringCellValue() + "\t");
+                            break;
+                        case NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "\t");
+                            break;
+                        case BOOLEAN:
+                            System.out.print(cell.getBooleanCellValue() + "\t");
+                            break;
+                        case FORMULA:
+                            System.out.print(cell.getCellFormula() + "\t");
+                            break;
+                        default:
+                            System.out.print("UNKNOWN\t");
+                    }
+                }
+                System.out.println();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
