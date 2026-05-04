c203a5d9f5d51fd28715d62189a6e1b28b2c92f6
Denisa Nicolaescu <mariadenisa.nicolaescu@ulbsibiu.ro>
2026-05-04 15:19:20 +0300

    Tema casa lab 8

--- src/MainStudentiImutabili.java
+++ src/MainStudentiImutabili.java
@@ -1,7 +1,14 @@
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import static org.apache.poi.ss.usermodel.CellType.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.*;
 
 public class MainStudentiImutabili {
 
@@ -21,6 +28,70 @@ public class MainStudentiImutabili {
         }
         return rezultat;
     }
+
+    public static void exportaStudenti(Set<StudentImutabil> studenti, String numeFisier) {
+        try (Workbook workbook = new HSSFWorkbook();
+             FileOutputStream fileOut = new FileOutputStream(numeFisier)) {
+
+            Sheet sheet = workbook.createSheet("Studenti");
+            int rowNum = 0;
+
+
+            Row headerRow = sheet.createRow(rowNum++);
+            headerRow.createCell(0).setCellValue("Nr Matricol");
+            headerRow.createCell(1).setCellValue("Nume");
+            headerRow.createCell(2).setCellValue("Prenume");
+            headerRow.createCell(3).setCellValue("Grupa");
+            headerRow.createCell(4).setCellValue("Nota");
+
+
+            for (StudentImutabil st : studenti) {
+                Row row = sheet.createRow(rowNum++);
+                row.createCell(0).setCellValue(st.getNumarMatricol());
+                row.createCell(1).setCellValue(st.getNume());
+                row.createCell(2).setCellValue(st.getPrenume());
+                row.createCell(3).setCellValue(st.getFormatie());
+                row.createCell(4).setCellValue(st.getNota());
+            }
+
+            workbook.write(fileOut);
+            System.out.println("Fisierul " + numeFisier + " a fost generat cu succes!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Set<StudentImutabil> citesteStudenti(String numeFisier) {
+        Set<StudentImutabil> studentiCititi = new LinkedHashSet<>();
+
+        try (FileInputStream fis = new FileInputStream(new File(numeFisier));
+             Workbook workbook = new HSSFWorkbook(fis)) {
+
+            Sheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+
+            if (rowIterator.hasNext()) rowIterator.next();
+
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+
+                int nrMatricol = (int) row.getCell(0).getNumericCellValue();
+                String nume = row.getCell(1).getStringCellValue();
+                String prenume = row.getCell(2).getStringCellValue();
+                String grupa = row.getCell(3).getStringCellValue();
+                float nota = (float) row.getCell(4).getNumericCellValue();
+
+                studentiCititi.add(new StudentImutabil(nrMatricol, prenume, nume, grupa, nota));
+            }
+
+        } catch (IOException e) {
+            System.err.println("Eroare la citire: " + e.getMessage());
+        }
+        return studentiCititi;
+    }
+
+
     public static void main(String[] args){
         Set<StudentImutabil> listaInitiala = new LinkedHashSet<>();
         listaInitiala.add(new StudentImutabil(1025, "Andrei", "Popa", "ISM141/2", 8.7f));
@@ -35,6 +106,16 @@ public class MainStudentiImutabili {
         for (StudentImutabil s : listaRepartizata) {
             System.out.println(s);
         }
+
+        String fisierXls = "laborator8_students.xls";
+        exportaStudenti(listaInitiala, fisierXls);
+
+        System.out.println(" Studenti cititi din fisierul .xls ");
+        Set<StudentImutabil> listaDinFisier = citesteStudenti(fisierXls);
+        for (StudentImutabil s : listaDinFisier) {
+            System.out.println(s);
+        }
+
     }
 
 
